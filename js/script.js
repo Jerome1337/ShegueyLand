bb0561cb35d6e0e65ac3cd1ec1918c4e8e07ec98
Jerome1337 <p-jerome@hotmail.fr>
2014-12-13 17:33:58 +0100

    Test boutons générateurs de punch

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// $("button").click(function(){
+// 	document.getElementById('sheguey').play();
+// });
+
+// function sheguey() {
+// 		document.getElementById('sheguey').play();
+// }
+
+// function missile() {
+// 		document.getElementById('missile').play();
+// }
+
+function playSheguey() {
+	zone = document.getElementById("audioElements");
+	tag = document.createElement('audio');
+	tag.setAttribute("preload","auto")
+	tag.setAttribute("autoplay","autoplay")
+	tag.setAttribute("src","snd/sheguey.wav")
+	tag.setAttribute("id","audio"+id)
+	zone.appendChild(tag)
+	tag.play()
+	setTimeout("x=document.getElementById('audio"+id+"');x.parentNode.removeChild(x);", 3500)
+	id++;
+	buttonClicked()
+}
+
+function playMissile() {
+	zone = document.getElementById("audioElements");
+	tag = document.createElement('audio');
+	tag.setAttribute("preload","auto")
+	tag.setAttribute("autoplay","autoplay")
+	tag.setAttribute("src","snd/missile.wav")
+	tag.setAttribute("id","audio"+id)
+	zone.appendChild(tag)
+	tag.play()
+	setTimeout("x=document.getElementById('audio"+id+"');x.parentNode.removeChild(x);", 3500)
+	id++;
+	buttonClicked()
+}
+
+function play59() {
+	zone = document.getElementById("audioElements");
+	tag = document.createElement('audio');
+	tag.setAttribute("preload","auto")
+	tag.setAttribute("autoplay","autoplay")
+	tag.setAttribute("src","snd/59.wav")
+	tag.setAttribute("id","audio"+id)
+	zone.appendChild(tag)
+	tag.play()
+	setTimeout("x=document.getElementById('audio"+id+"');x.parentNode.removeChild(x);", 3500)
+	id++;
+	buttonClicked()
+}
+
+function playPapa() {
+	zone = document.getElementById("audioElements");
+	tag = document.createElement('audio');
+	tag.setAttribute("preload","auto")
+	tag.setAttribute("autoplay","autoplay")
+	tag.setAttribute("src","snd/papa.wav")
+	tag.setAttribute("id","audio"+id)
+	zone.appendChild(tag)
+	tag.play()
+	setTimeout("x=document.getElementById('audio"+id+"');x.parentNode.removeChild(x);", 3500)
+	id++;
+	buttonClicked()
+}
+
+function playJmatte() {
+	zone = document.getElementById("audioElements");
+	tag = document.createElement('audio');
+	tag.setAttribute("preload","auto")
+	tag.setAttribute("autoplay","autoplay")
+	tag.setAttribute("src","snd/jmatte.wav")
+	tag.setAttribute("id","audio"+id)
+	zone.appendChild(tag)
+	tag.play()
+	setTimeout("x=document.getElementById('audio"+id+"');x.parentNode.removeChild(x);", 3500)
+	id++;
+	buttonClicked()
+}
