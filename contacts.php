170d23bdc6f167efe08fe67308ca9d0cc883da10
Jérôme Pogeant <p-jerome@hotmail.fr>
2014-12-21 12:00:21 +0100

    Ajout Twitter API avec les dernier tweets qui s'affichent, text sur la page générateur de son

--- contacts.php
+++ contacts.php
@@ -16,7 +16,30 @@
         
         <div class="row">
             <div class="large-12 columns">
-                
+                <?php
+                    $cache = 'cache/tweets.tmp';
+                    if(time() - filemtime($cache) > 60){
+                        require 'class/twitteroauth.php';
+                        $connection = new TwitterOAuth('ABScq6XifMwUz1zhJVHxlyEeg', 'q3ulKblNbaUTs0jiLjV9uhcoN0uBe1rOdcxS9EwjuWzT7jZkae', '179239110-GarudRbmtNpNMS1ApCx5DmTAuV3NzCeUs4gdoZcq', 'rPdDESMAb74NkVnOfMOjowUXhr5iCMuCyyerfkKhJIfhe');
+                        $tweets = $connection->get('statuses/user_timeline', array('user_id' => 1585757612), array('exclude_replies' => true));
+                        file_put_contents($cache, serialize($tweets));
+                        // var_dump($tweets);
+                    }else{
+                        echo "Cache utilisé";
+                        $tweets = unserialize(file_get_contents($cache));
+                    }
+                ?>
+                <ul class="pricing-table">
+                <?php foreach ($tweets as $k => $tweet): ?>
+                    <li class="bullet-item"><?= $tweet->text; ?></li>
+                <?php endforeach ?>
+                    <!-- <li>
+                        <form action="#">
+                            <input type="text" value="@GRADIDUR">
+                            <button type="submit">Poster</button>
+                        </form>
+                    </li> -->
+                </ul>
             </div>
         </div>
         <?php include('commons/footer.php'); ?>
--- shegueynoise.php
+++ shegueynoise.php
@@ -1,36 +1,56 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <?php 
+    <head>
+        <?php
         $onglet_actif = "noise";
         include('commons/head.php'); ?>
         <title>SHEGUEYLAND</title>
-  </head>
-  <body>
-    <?php include('commons/nav.php'); ?>
-    <div class="row">
-      <div class="large-12 columns">
-        <h1>Sheguey Noise</h1>
-      </div>
-    </div>
-    
-    <div class="row">
-      <div class="large-12 columns">
-        <button onclick="playSheguey()" class="medium-4 large-4 columns">SHEGUEY</button>
-        <button onclick="playMissile()" class="medium-4 large-4 columns">MISSILE SOL-AIR</button>
-        <button onclick="play59()" class="medium-4 large-4 columns">5.9 KILS DE PURE</button>
-        <button onclick="play59()" class="medium-4 large-4 columns">5.9 KILS DE PURE</button>
-        <button onclick="playPapa()" class="medium-4 large-4 columns">PAPA VEUT DU BIFF</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-        <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
-      </div>
-    </div>
-    <?php include('commons/footer.php'); ?>
-    <?php include('commons/script_bottom.php'); ?>
-  </body>
+    </head>
+    <body>
+        <?php include('commons/nav.php'); ?>
+        <div class="row">
+            <div class="large-12 columns">
+                <h1>Sheguey Noise</h1>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="large-12 columns">
+                <div class="conteneur">
+                    <div onclick="playSheguey()" class="medium-4 large-4 columns"><img src="img/generateur/sheguey.jpg" alt="Sheguey Button"></div>
+                    <div onclick="playMissile()" class="medium-4 large-4 columns"><img src="img/generateur/terrasser.png" alt="Sheguey Button"></div>
+                    <div onclick="playMissile()" class="medium-4 large-4 columns"><img src="img/generateur/dani.jpg" alt="Sheguey Button"></div>
+                </div>
+                <div class="conteneur">
+                    <div onclick="play59()" class="medium-4 large-4 columns"><img src="img/generateur/sheguey.jpg" alt="Sheguey Button"></div>
+                    <div onclick="play59()" class="medium-4 large-4 columns"><img src="img/generateur/terrasser.png" alt="Sheguey Button"></div>
+                    <div onclick="play59()" class="medium-4 large-4 columns"><img src="img/generateur/dani.jpg" alt="Sheguey Button"></div>
+                </div>
+                <div class="conteneur">
+                    <div onclick="playPapa()" class="medium-4 large-4 columns"><img src="img/generateur/sheguey.jpg" alt="Sheguey Button"></div>
+                    <div onclick="playPapa()" class="medium-4 large-4 columns"><img src="img/generateur/terrasser.png" alt="Sheguey Button"></div>
+                    <div onclick="playPapa()" class="medium-4 large-4 columns"><img src="img/generateur/dani.jpg" alt="Sheguey Button"></div>
+                </div>
+                <div class="conteneur">
+                    <div onclick="playJmatte()" class="medium-4 large-4 columns"><img src="img/generateur/sheguey.jpg" alt="Sheguey Button"></div>
+                    <div onclick="playJmatte()" class="medium-4 large-4 columns"><img src="img/generateur/terrasser.png" alt="Sheguey Button"></div>
+                    <div onclick="playJmatte()" class="medium-4 large-4 columns"><img src="img/generateur/dani.jpg" alt="Sheguey Button"></div>
+                </div>
+                <!--  <button onclick="playSheguey()" class="medium-4 large-4 columns">SHEGUEY</button>
+                <button onclick="playMissile()" class="medium-4 large-4 columns">MISSILE SOL-AIR</button>
+                <button onclick="play59()" class="medium-4 large-4 columns">5.9 KILS DE PURE</button>
+                <button onclick="play59()" class="medium-4 large-4 columns">5.9 KILS DE PURE</button>
+                <button onclick="playPapa()" class="medium-4 large-4 columns">PAPA VEUT DU BIFF</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button>
+                <button onclick="playJmatte()" class="medium-4 large-4 columns">J'MATTE LE CUL D'TA RACLI</button> -->
+            </div>
+        </div>
+        <?php include('commons/footer.php'); ?>
+        <?php include('commons/script_bottom.php'); ?>
+    </body>
 </html>
