5d00de0bfd3ce9b4cc37c3615e430d8b9f41d53b
jo2selin <josselin.caer@gmail.com>
2015-01-23 18:20:34 +0100

    [UPDATED] Menu Home

--- app/adm/bddconnect.php
+++ app/adm/bddconnect.php
@@ -1,7 +1,7 @@
 <?php
 	$host = 'mysql:dbname=Sheguey;host=localhost;charset:UFT-8';
 	$user = 'root';
-	$pass = 'root';
+	$pass = '';
 	try{
 		$bdd = new PDO($host, $user, $pass);
 	}catch (PDOException $e){
---
+++ dist/commons/test.php
@@ -0,0 +1,7 @@
+<?php
+	$data = array();
+	if (!empty($_POST['name'])
+	{	$data['success'] = true;
+	}else{
+		$data['success'] = false;
+	}
