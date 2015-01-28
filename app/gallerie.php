c34547ad316a7348e5de132210081d509c83d35d
jo2selin <josselin.caer@gmail.com>
2015-01-28 13:34:48 +0100

    [UPDATED] gallerie

--- app/gallerie.php
+++ app/gallerie.php
@@ -33,7 +33,8 @@
         <?php include('commons/script_bottom.php'); ?>
         <script>
             $(document).ready(function() {
-                loadGallery("both", "DESC"); // Chargement de la gallerie au 1er chargement
+                loadGallery("both", "recent"); // Chargement de la gallerie au 1er chargement
+                $('.mediaContent').prop('checked', true); // Coche les 2 case si non defini
             });
         </script>
     </body>
